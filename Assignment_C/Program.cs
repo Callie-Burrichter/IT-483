3fd2d72362cb6488d192518cf0863684915425a6
Callie-Burrichter <60206394+Callie-Burrichter@users.noreply.github.com>
2020-04-25 11:50:46 -0500

    Add files via upload

---
+++ Assignment_C/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AssignmentC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lab2 mylab2 = new Lab2();
+            //Lab1 mylab1 = new Lab1();
+        }
+    }
+}
